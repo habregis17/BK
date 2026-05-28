f36aa6b6c3b5129081ce0556615a112727e78afb
HABUWITEKA FRancois Regis <habregis17@gmail.com>
2026-05-28 09:24:43 +0200

    View changes

--- Admin/cases/view.php
+++ Admin/cases/view.php
@@ -1,5 +1,4 @@
 <?php
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -9,7 +8,6 @@ $pageTitle = 'Case View';
 require '../includes/header.php';
 require '../includes/sidebar.php';
 require '../../config/db.php';
-
 
 $caseNumber = $_GET['casenumber'] ?? '';
 if (!$caseNumber) {
